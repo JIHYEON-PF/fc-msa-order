3eadc4d4f4a87afe787f9e48ee100e1fcf4357a3
JIHYEON <jhkim@aptner.com>
2023-03-16 00:20:18 +0900

    feature:Partner 서비스 구현
    - 주문 프로젝트의 Partner 도메인의 서비스를 구현
    - Domain Layer에 Interface를 구현
    - Infrastructure Layer에 상세 서비스를 구현
      - Domain Layer에는 `@Service`를 선언하여 Entry Point를 명시하고, Infra Layer에는 `@Component`를 선언하여 구현부를 명시
    - 공용으로 사용하게 될 Error 코드 선언

---
+++ src/main/java/dev/project/order/common/exception/EntityNotFoundException.java
@@ -0,0 +1,14 @@
+package dev.project.order.common.exception;
+
+import dev.project.order.common.response.ErrorCode;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
